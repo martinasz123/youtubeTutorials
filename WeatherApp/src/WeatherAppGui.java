df6e8a10f7e3943a2208991f3e12313e49ae419d
MartinSomogyi <martinasz123@gmail.com>
2025-02-06 13:32:16 +0000

    Initial Commit

---
+++ WeatherApp/src/WeatherAppGui.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame{
+    public WeatherAppGui(){
+        //Setting up gui and adding title
+        super("Weather app");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(450, 650);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+
+    }
+}
